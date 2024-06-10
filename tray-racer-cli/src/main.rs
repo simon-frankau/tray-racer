48c8391cdf9bef6b772b6bc0f48f80d738ff5bef
Simon Frankau <sgf@arbitrary.name>
2024-06-10 15:09:08 +0100

    Simplify/tidy-up/inline code in tray-racer-cli.

--- tray-racer-cli/src/main.rs
+++ tray-racer-cli/src/main.rs
@@ -7,8 +7,8 @@ use std::path::Path;
 
 use anyhow::*;
 use clap::Parser;
-use image::RgbaImage;
 use image::imageops::flip_vertical_in_place;
+use image::RgbaImage;
 
 use tray_racer_lib::{CanvasConfig, EnvMap, Tracer};
 
@@ -38,73 +38,51 @@ struct Args {
 // Main code.
 //
 
-const WIDTH: u32 = 1024;
-const HEIGHT: u32 = 768;
+const WIDTH: usize = 1024;
+const HEIGHT: usize = 768;
 
-struct Drawable {
-    tracer: Tracer,
-    tilt: f64,
-    turn: f64,
-    pan: f64,
-    fov: f64,
-}
-
-impl Drawable {
-    fn new(env_map_path_pos: &Path, env_map_path_neg: &Path) -> Drawable {
-        let env_map_pos = EnvMap::from(env_map_path_pos).unwrap();
-        let env_map_neg = EnvMap::from(env_map_path_neg).unwrap();
-
-        Drawable {
-            tracer: Tracer {
-                env_map_pos,
-                env_map_neg,
-                w_scale: 0.25,
-                radius: 0.1,
-                infinity: 4.0,
-            },
-            tilt: 0.0,
-            turn: 0.0,
-            pan: 0.0,
-            fov: 90.0,
-        }
-    }
-
-    /* TODO: Check ranges on inputs
-               .add(egui::Slider::new(&mut self.fov, 20.0..=160.0).text("Field of view"))
-               .add(egui::Slider::new(&mut self.tilt, -90.0..=90.0).text("Tilt"))
-               .add(egui::Slider::new(&mut self.turn, -180.0..=180.0).text("Turn"))
-               .add(egui::Slider::new(&mut self.pan, -180.0..=180.0).text("Pan"))
-               .add(egui::Slider::new(&mut self.tracer.radius, -1.0..=1.0).text("Wormhole radius"))
-               .add(egui::Slider::new(&mut self.tracer.w_scale, 0.1..=1.0).text("Smoothness"))
-               .add(egui::Slider::new(&mut self.tracer.infinity, 1.0..=10.0).text("Infinity"))
-    */
-
-    fn render(&self) -> Vec<u8> {
-        // TODO
-        let (w, h) = (1024, 768);
-
-        self.tracer.render(
-            &CanvasConfig {
-                width: w,
-                height: h,
-                aspect: 1.0,
-                fov_degrees: self.fov,
-            },
-            self.tilt,
-            self.turn,
-            self.pan,
-        )
-    }
-}
+/* TODO: Check ranges on inputs
+           .add(egui::Slider::new(&mut self.fov, 20.0..=160.0).text("Field of view"))
+           .add(egui::Slider::new(&mut self.tilt, -90.0..=90.0).text("Tilt"))
+           .add(egui::Slider::new(&mut self.turn, -180.0..=180.0).text("Turn"))
+           .add(egui::Slider::new(&mut self.pan, -180.0..=180.0).text("Pan"))
+           .add(egui::Slider::new(&mut self.tracer.radius, -1.0..=1.0).text("Wormhole radius"))
+           .add(egui::Slider::new(&mut self.tracer.w_scale, 0.1..=1.0).text("Smoothness"))
+           .add(egui::Slider::new(&mut self.tracer.infinity, 1.0..=10.0).text("Infinity"))
+*/
 
 fn main() -> Result<()> {
     let args = Args::parse();
 
-    let drawable = Drawable::new(Path::new(&args.env_map_pos), Path::new(&args.env_map_neg));
+    let env_map_pos = EnvMap::from(Path::new(&args.env_map_pos))?;
+    let env_map_neg = EnvMap::from(Path::new(&args.env_map_neg))?;
+    let tracer = Tracer {
+        env_map_pos,
+        env_map_neg,
+        w_scale: 0.25,
+        radius: 0.1,
+        infinity: 4.0,
+    };
 
-    let raw_image = drawable.render();
-    let mut image =
-        RgbaImage::from_raw(WIDTH, HEIGHT, raw_image).ok_or(anyhow!("Couldn't create image"))?;
+    let tilt = 0.0;
+    let turn = 0.0;
+    let pan = 0.0;
+    let fov = 90.0;
+
+    let raw_image = tracer.render(
+        &CanvasConfig {
+            width: WIDTH,
+            height: HEIGHT,
+            aspect: 1.0,
+            fov_degrees: fov,
+        },
+        tilt,
+        turn,
+        pan,
+    );
+
+    let mut image = RgbaImage::from_raw(WIDTH as u32, HEIGHT as u32, raw_image)
+        .ok_or(anyhow!("Couldn't create image"))?;
     // OpenGL uses inverted vertical axis.
     flip_vertical_in_place(&mut image);
     image.save(args.output)?;
