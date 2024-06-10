005612232e25d3e76c9d81f077597ddbf0314a80
Simon Frankau <sgf@arbitrary.name>
2024-06-10 15:24:21 +0100

    Make tilt, turn, pan and fov configurable in the CLI.

--- tray-racer-cli/src/main.rs
+++ tray-racer-cli/src/main.rs
@@ -32,6 +32,18 @@ struct Args {
     /// File to write the output to
     #[arg(short, long)]
     output: String,
+    /// Camera 'pitch', in degrees
+    #[arg(long, default_value_t = 0.0)]
+    tilt: f64,
+    /// Camera 'yaw', in degrees
+    #[arg(long, default_value_t = 0.0)]
+    turn: f64,
+    /// Angle turned around the wormhole, in degrees
+    #[arg(long, default_value_t = 0.0)]
+    pan: f64,
+    /// Horizontal camera field of view, in degrees
+    #[arg(long, default_value_t = 90.0)]
+    fov: f64,
 }
 
 ////////////////////////////////////////////////////////////////////////
@@ -42,18 +54,18 @@ const WIDTH: usize = 1024;
 const HEIGHT: usize = 768;
 
 /* TODO: Check ranges on inputs
-           .add(egui::Slider::new(&mut self.fov, 20.0..=160.0).text("Field of view"))
-           .add(egui::Slider::new(&mut self.tilt, -90.0..=90.0).text("Tilt"))
-           .add(egui::Slider::new(&mut self.turn, -180.0..=180.0).text("Turn"))
-           .add(egui::Slider::new(&mut self.pan, -180.0..=180.0).text("Pan"))
            .add(egui::Slider::new(&mut self.tracer.radius, -1.0..=1.0).text("Wormhole radius"))
            .add(egui::Slider::new(&mut self.tracer.w_scale, 0.1..=1.0).text("Smoothness"))
            .add(egui::Slider::new(&mut self.tracer.infinity, 1.0..=10.0).text("Infinity"))
+   Other configurable values:
+      image width
+      image height
+      ray step size
 */
 
 fn main() -> Result<()> {
     let args = Args::parse();
-
+    
     let env_map_pos = EnvMap::from(Path::new(&args.env_map_pos))?;
     let env_map_neg = EnvMap::from(Path::new(&args.env_map_neg))?;
     let tracer = Tracer {
@@ -64,15 +76,20 @@ fn main() -> Result<()> {
         infinity: 4.0,
     };
 
-    let tilt = 0.0;
-    let turn = 0.0;
-    let pan = 0.0;
-    let fov = 90.0;
+    let fov = args.fov;
+    assert!(20.0 <= fov && fov <= 160.0);
+    let tilt = args.tilt;
+    assert!(-90.0 <= tilt && tilt <= 90.0);
+    let turn = args.turn;
+    assert!(-180.0 <= turn && turn <= 180.0);
+    let pan = args.pan;
+    assert!(-180.0 <= pan && pan <= 180.0);
 
     let raw_image = tracer.render(
         &CanvasConfig {
             width: WIDTH,
             height: HEIGHT,
+	    // When writing out an image, we'll always assume square pixels.
             aspect: 1.0,
             fov_degrees: fov,
         },
